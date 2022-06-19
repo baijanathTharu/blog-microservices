0270e31dddf5fd34462a6bee8346dcbf88363bab
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-06-19 18:24:49 +0545

    init

---
+++ posts/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let posts = {};
+
+// get posts
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+// post posts
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Server started on port 4000');
+});
