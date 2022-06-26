74da144c16a5093efbe97628f73db158e82c3fa5
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-06-26 18:13:49 +0545

    add comments service

---
+++ comments/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+const commentsByPostId = {};
+
+app.use(bodyParser.json());
+
+app.get('/posts/:id/comments', (req, res) => {
+  const postId = req.params.id;
+  res.send(commentsByPostId[postId] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+
+  const content = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({
+    id: commentId,
+    content,
+  });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).json(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Comment Service Listening on port 4001');
+});
